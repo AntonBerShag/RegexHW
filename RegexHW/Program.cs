7457f32837e2b636c44e820269493dde5a9c160d
Berd_ur18@itstepru.onmicrosoft.com <Berd_ur18@itstepru.onmicrosoft.com>
2022-11-07 01:40:03 +0500

    Add project files.

---
+++ RegexHW/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexProgram regexP = new RegexProgram();
+            regexP.massage();
+            regexP.CiscoToWin();
+            Console.WriteLine("Преобразование...");
+            regexP.massage();
+        }
+    }
+}
---
+++ RegexHW/RegexProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexHW
+{
+    internal class RegexProgram
+    {
+        private string macAdressCisco = "901b.0e94.83a8";
+        private string macAdressWin;
+
+        public string CiscoToWin()
+        {
+            Regex regex = new Regex(@"[0-9a-z]{1,4}");
+            MatchCollection matches = regex.Matches(macAdressCisco);
+            foreach (Match match in matches)
+            {
+                macAdressWin += match.Value;
+            }
+            macAdressWin = macAdressWin.ToUpper();
+            macAdressWin = Regex.Replace(macAdressWin, ".{2}", "$0-");
+            macAdressWin = macAdressWin.TrimEnd('-');
+            return macAdressWin;
+        }
+        public void massage()
+        {
+            Console.WriteLine("MAC address Cisco имеет вид: {0}", macAdressCisco);
+            Console.WriteLine("MAC address Windows имеет вид: {0}", macAdressWin);
+        }
+    }
+}
